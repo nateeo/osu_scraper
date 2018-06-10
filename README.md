40573460a170e8df15a9d0c830504c7422a4be0f
nateeo <hur.nathan@gmail.com>
2018-06-11 00:58:49 +1200

    :rocket:

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple python script to scrape osu leaderboard's and each user's profile (currently only getting their playtime)
+
+uses `urllib3` and `bs4` (BeautifulSoup)
---
+++ scrape.py
@@ -0,0 +1,47 @@
+import csv
+import urllib3
+from datetime import datetime
+import time
+from bs4 import BeautifulSoup
+
+RANK_PAGE = 'https://osu.ppy.sh/rankings/osu/performance?country=NZ&page='
+MAX_PAGE = 2
+
+CSV_FILE = 'osu_data.csv'
+
+http = urllib3.PoolManager()
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def get_play_time(text):
+    sub = text[text.find('play_time') + 11:]
+    return sub[:sub.find(',')]
+
+
+for i in range(1, MAX_PAGE):
+    # save per page in case something happens
+    with open(CSV_FILE, 'a') as csv_file:
+        start = time.time()
+        writer = csv.writer(csv_file)
+        page = http.request('GET', RANK_PAGE + str(i))
+        parsed_page = BeautifulSoup(page.data, 'html.parser')
+        rows = parsed_page.find_all(
+            'tr', attrs={'class': 'ranking-page-table__row'})
+
+        for row in rows:
+            cols = row.find_all('td', {'class': 'ranking-page-table__column'})
+            links = cols[1].find_all('a', href=True)
+            user_profile_link = links[1]['href'].strip()
+            user_row = []
+            for val in cols:
+                user_row.append(val.text.strip())
+
+            profile = http.request('GET', user_profile_link)
+            profile_text = profile.data.decode('utf-8')
+            profile.close()
+            user_row.append(get_play_time(profile_text))
+            writer.writerow(user_row)
+
+        print('processed page', i, ' time-taken: ', time.time() - start)
+        page.close()
